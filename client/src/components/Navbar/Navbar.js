e5b6d8ebde7f17a395def74d0d4959d469b9813f
Nur A Alam <nuraalam376@gmail.com>
2021-06-07 02:32:30 +0600

    added cart count

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CartContext } from "../../App";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [cartItems] = useContext(CartContext);
   return (
     <div id="navbar">
       <ul id="navbar-items">
-        <li className="navbar-item">
-          <h2>E-Shop</h2>
-        </li>
+        <li className="navbar-item logo">E-Shop</li>
         <li className="navbar-item" id="cart">
-          Cart
+          <i className="fa fa-shopping-cart cart-icon" aria-hidden="true"></i>
+          <span id="cartItem">{cartItems.length}</span>
         </li>
+        <li className="clearfix"></li>
       </ul>
     </div>
   );
