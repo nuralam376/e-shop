65409a003f97a45d1d48cd1091b112e2489dabd2
Nur A Alam <nuraalam376@gmail.com>
2021-06-06 20:29:16 +0600

    get all products

--- server/app.js
+++ server/app.js
@@ -4,15 +4,26 @@ const sequelize = require("./db/db");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const Product = require("./models/Product");
+const ProductRoutes = require("./routes/ProductRoutes");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/products", ProductRoutes);
 
 sequelize
   .sync()
   .then(() => {
     console.log("Database connected");
     app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+    // Inserted fake products
+    // const fakeProducts = require("./fakeProducts");
+    //   fakeProducts.forEach((product) => {
+    //     Product.create(product)
+    //       .then(() => console.log("Products data saved"))
+    //       .catch((err) => console.log(err));
+    //   });
   })
   .catch((err) => console.log(err));
 
---
+++ server/fakeProducts.js
@@ -0,0 +1,17 @@
+const products = [
+  {
+    title: "A Book",
+    image:
+      "https://www.publicdomainpictures.net/pictures/10000/velka/1-1210009435EGmE.jpg",
+    price: "19",
+  },
+  {
+    title: "Second Book",
+    image:
+      "https://www.publicdomainpictures.net/pictures/10000/velka/1-1210009435EGmE.jpg",
+
+    price: "12",
+  },
+];
+
+module.exports = products;
