e00c91cd7c5364ec3f98c613951faf96a4b3b6b2
Nur A Alam <nuraalam376@gmail.com>
2021-06-06 21:39:35 +0600

    added cors

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import axios from "axios";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+
+axios.defaults.baseURL = "http://localhost:5000/api";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const sequelize = require("./db/db");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -8,6 +9,7 @@ const ProductRoutes = require("./routes/ProductRoutes");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use("/api/products", ProductRoutes);
 
