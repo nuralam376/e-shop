92486fcae9032e09556a4368a4d456876b566713
Nur A Alam <nuraalam376@gmail.com>
2021-06-06 22:04:04 +0600

    added cart added buttons

--- client/src/components/Product/Product.js
+++ client/src/components/Product/Product.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Product.css";
 
 const Product = ({ product }) => {
-  const { image, title, price } = product;
+  const { id, image, title, price } = product;
+
+  const [cartId, setCartId] = useState(null);
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCartId(id);
+    setCount(1);
+  };
+
+  const handleCount = (type) => {
+    if (type === "add") {
+      setCount(count + 1);
+    } else if (type === "substract" && count > 0) {
+      setCount(count - 1);
+    }
+  };
+
   return (
     <>
       <img src={image} alt={title} />
       <h3>{title}</h3>
       <p>৳{price}</p>
-      <button className="cart-button">Add to Cart</button>
+      {!cartId ? (
+        <button className="cart-button" onClick={handleClick}>
+          Add to Cart
+        </button>
+      ) : (
+        <div className="count-item-cart">
+          <button onClick={() => handleCount("substract")}>-</button>
+          <p>{count}</p>
+          <button onClick={() => handleCount("add")}>+</button>
+        </div>
+      )}
     </>
   );
 };
