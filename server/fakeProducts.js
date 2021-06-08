2b89c02edef66f4a4c70f623127eceae9f26e380
Nur A Alam <nuraalam376@gmail.com>
2021-06-08 11:43:56 +0600

    added fake products

--- server/fakeProducts.js
+++ server/fakeProducts.js
@@ -1,16 +1,37 @@
 const products = [
   {
-    title: "A Book",
+    title: "Eloquent JavaScript",
     image:
-      "https://www.publicdomainpictures.net/pictures/10000/velka/1-1210009435EGmE.jpg",
-    price: "19",
+      "https://images-na.ssl-images-amazon.com/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg",
+    price: "300",
   },
   {
-    title: "Second Book",
+    title: "JavaScript and JQuery",
     image:
-      "https://www.publicdomainpictures.net/pictures/10000/velka/1-1210009435EGmE.jpg",
+      "https://images-na.ssl-images-amazon.com/images/I/31SRWF+LkKL._SX258_BO1,204,203,200_.jpg",
 
-    price: "12",
+    price: "500",
+  },
+  {
+    title: "JavaScript: The Definitive Guide",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/51wijnc-Y8L._SX379_BO1,204,203,200_.jpg",
+
+    price: "700",
+  },
+  {
+    title: "Node.js Design Patterns",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/515wgVc7S9L._SX404_BO1,204,203,200_.jpg",
+
+    price: "200",
+  },
+  {
+    title: "The Road to React",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/41AJiI9AyyL._SX384_BO1,204,203,200_.jpg",
+
+    price: "100",
   },
 ];
 
